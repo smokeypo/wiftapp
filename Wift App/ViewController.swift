59f4740cea1a205cb83c157a03d18362b05f9a13
Neil Johnson <neil@andalsosmokeypo.com>
2016-10-26 14:57:29 +1100

    my second commit

--- Wift App/ViewController.swift
+++ Wift App/ViewController.swift
@@ -24,8 +24,8 @@ class ViewController: UIViewController {
         
         tapCount = tapCount + 1
        
-        if tapCount  >= 10 {
-            theLabel.text = "You Tapped the button 10 times"
+        if tapCount  >= 20 {
+            theLabel.text = "You Tapped the button 20 times"
         }
     }
     
